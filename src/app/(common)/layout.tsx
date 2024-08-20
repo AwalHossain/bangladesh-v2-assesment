9e5c1cb710c083d2276e696f8d5118268b22305a
Awalhossain <waqin21@gmail.com>
2024-08-20 14:28:29 +0600

    chore: Add AuthPageLayout component

---
+++ src/app/(common)/layout.tsx
@@ -0,0 +1,16 @@
+import { ChildrenProps } from '@/lib/types'
+
+export default function AuthPageLayout({ children }: ChildrenProps) {
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md sm:max-w-2xl md:max-w-4xl lg:max-w-6xl bg-white rounded-lg shadow-lg p-4 md:p-8">
+                <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">Build Your Regimen</h1>
+
+                <div className="w-full h-[2px] bg-[#EEE] my-2"></div>
+                <div className="">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
