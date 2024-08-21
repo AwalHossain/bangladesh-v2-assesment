5a8b33a4b9942c06e77a5a170ce4db20f0c48d0f
Awalhossain <waqin21@gmail.com>
2024-08-21 10:28:36 +0600

    refactor: Update AuthPageLayout component to include Navbar and remove unnecessary code

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -1,12 +1,12 @@
+import Navbar from '@/components/ui/nav'
 import { ChildrenProps } from '@/lib/types'
 
 export default function AuthPageLayout({ children }: ChildrenProps) {
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md sm:max-w-2xl md:max-w-4xl lg:max-w-6xl bg-white rounded-lg shadow-lg p-4 md:p-8">
-                <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">Bijoy 2024</h1>
-
-                <div className="w-full h-[2px] bg-[#EEE] my-2"></div>
+            <div className="w-full max-w-md sm:max-w-2xl md:max-w-4xl lg:max-w-6xl bg-white rounded-lg shadow-lg">
+                {/* <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">Bijoy 2024</h1> */}
+                <Navbar />
                 <div className="">
                     {children}
                 </div>
