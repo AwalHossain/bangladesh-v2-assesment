e590422872b53566169a8ee5efa814361f2198ac
Awalhossain <waqin21@gmail.com>
2024-08-21 11:15:24 +0600

    refactor: Update zipCode validation in RegistrationFormDataSchema

--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -2,13 +2,15 @@ import { z } from 'zod';
 
 export const LoginFormDataSchema = z.object({
   email: z.string().email('Invalid email address').min(1, 'Email is required'),
-  password: z.string().min(4, 'Password is required'),
+  password: z.string().min(6, 'Password is required'),
 })
 
 export const RegistrationFormDataSchema = z.object({
   fullName: z.string().min(1, { message: 'Name is required' }),
   email: z.string().email({ message: 'Invalid email address' }).min(1, { message: 'Email is required' }),
-  zipCode: z.number().min(4, { message: 'Zip Code is required' }),
+  zipCode: z.string().min(4, { message: 'Zip Code is required' })
+  .transform((val) => parseInt(val, 10))
+  .refine((val) => !isNaN(val) && val > 0, { message: 'Invalid Zip Code' }),
   password: z.string().min(6, { message: 'Password is required' }),
   confirmPassword: z.string().min(6, { message: 'Confirm Password is required' }),
 }).refine(data => data.password === data.confirmPassword, {
