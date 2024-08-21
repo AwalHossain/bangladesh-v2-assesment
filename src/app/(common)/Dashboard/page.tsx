a8b4a528c7a5a2ef1eddeae247a47f63dac029d7
Awalhossain <waqin21@gmail.com>
2024-08-21 08:42:38 +0600

    feat: Update UserTable component to fetch and display user data in a table format

--- src/app/(common)/Dashboard/page.tsx
+++ src/app/(common)/Dashboard/page.tsx
@@ -1,8 +1,106 @@
+"use client";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { IUser } from "@/lib/types";
+import { useEffect, useState } from "react";
 
-export default function Table() {
+const invoices = [
+    {
+        invoice: "INV001",
+        paymentStatus: "Paid",
+        totalAmount: "$250.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV002",
+        paymentStatus: "Pending",
+        totalAmount: "$150.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV003",
+        paymentStatus: "Unpaid",
+        totalAmount: "$350.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV004",
+        paymentStatus: "Paid",
+        totalAmount: "$450.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV005",
+        paymentStatus: "Paid",
+        totalAmount: "$550.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV006",
+        paymentStatus: "Pending",
+        totalAmount: "$200.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+]
+
+export default function UserTable() {
+    // fetch data from jsonplaceholder.typicode.com/users
+    const [users, setUsers] = useState([]);
+    const fetchUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }
+        , []);
+
     return (
-        <div>
-            <h1>Fetching Data</h1>
-        </div>
+        <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">Name</TableHead>
+                    <TableHead>Phone</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead className="text-right">City</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {users?.map((user: IUser) => (
+                    <TableRow key={user?.id}>
+                        <TableCell className="font-medium">{user?.name}</TableCell>
+                        <TableCell>{user?.phone}</TableCell>
+                        <TableCell>{user?.email}</TableCell>
+                        <TableCell className="text-right">{user?.address?.city}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+            <TableFooter>
+                <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                </TableRow>
+            </TableFooter>
+        </Table>
     )
 }
+
+
