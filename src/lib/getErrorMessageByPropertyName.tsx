9ee9320261d489f04380654b1db961b2f68c14e4
Awalhossain <waqin21@gmail.com>
2024-08-21 07:57:51 +0600

    feat: Add getErrorMessageByPropertyName utility function

---
+++ src/lib/getErrorMessageByPropertyName.tsx
@@ -0,0 +1,22 @@
+export const getErrorMessageByPropertyName = (obj: Record<string, any>, propertyPath: string) => {
+
+    const properties = propertyPath.split(".")
+
+    let value = obj;
+
+    console.log("Value of eerror", value);
+
+
+    for (let prop of properties) {
+        if (value[prop]) {
+            value = value[prop]
+        }
+        else {
+            return undefined;
+        }
+    }
+
+    return value.message;
+
+
+}
